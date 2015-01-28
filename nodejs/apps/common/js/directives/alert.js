a6442348228251955619039d85b215265faa8c0c
Timo Furrer <tuxtimo@gmail.com>
2015-01-28 10:15:21 +0100

    update

---
+++ nodejs/apps/common/js/directives/alert.js
@@ -0,0 +1,23 @@
+(function(module) {
+
+    var alert = function() {
+
+        // DDO (directive dinfition object
+        return {
+            restrict: 'E',
+            templateUrl: '/common/templates/alert.html',
+            transclude: true,
+            link: function(scope, element, attributes) {
+                var type = attributes.type || 'warning';
+                element.children().addClass('alert alert-' + type);
+
+                var closeSpan = element[0].querySelector('span.close');
+                angular.element(closeSpan).on('click', function() {
+                    element.remove();
+                });
+            }
+        };
+    };
+
+    module.directive('alert', alert);
+}(angular.module('common')));
